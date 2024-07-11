751004ac4914fbf339e311d9f4171192a3da9b3b
Giorgio Angelotti <g.angelotti91@gmail.com>
2024-07-11 12:04:08 +0200

    MultiOS conda compatibility fix

--- vesuvius/__init__.py
+++ vesuvius/__init__.py
@@ -3,7 +3,7 @@ import sys
 import site
 
 def check_agreement():
-    install_path = site.getsitepackages()[1]
+    install_path = site.getsitepackages()[-1]
     agreement_file_path = os.path.join(install_path, 'vesuvius', 'setup', 'agreement.txt')
 
     # Get the name of the currently running script
@@ -12,12 +12,12 @@ def check_agreement():
     # Check if the current script is not the setup script
     if not os.path.exists(agreement_file_path):
         if "accept_terms" not in current_script:
-            raise ImportError("You must accept the terms and conditions before using this package. Run `accept_terms.py`.")
+            raise ImportError("You must accept the terms and conditions before using this package. Run `vesuvius.accept_terms`.")
     else:   
         with open(agreement_file_path, 'r') as file:
             content = file.read().strip()
             if content != 'yes':
-                raise ImportError("The agreement file is corrupted or incorrect. Please run `accept_terms.py` again.")
+                raise ImportError("The agreement file is corrupted or incorrect. Please run `vesuvius.accept_terms` again.")
 
 # Check agreement on import
 check_agreement()
