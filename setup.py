12df9c08c4749b8016f7488f4d338624ac6fee27
Giorgio Angelotti <giorgio@scrollprize.org>
2024-08-09 14:15:15 +0200

    0.1.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='vesuvius',
-    version='0.1.2',
+    version='0.1.3',
     package_dir = {"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
@@ -35,6 +35,7 @@ setup(
         'nest_asyncio',
         'pynrrd',
         'pyyaml',
+        'Pillow'
     ],
     python_requires='>=3.8',
     include_package_data=True,
